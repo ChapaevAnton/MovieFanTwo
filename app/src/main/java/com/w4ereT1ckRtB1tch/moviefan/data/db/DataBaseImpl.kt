b5d4c7788a1b076c464fb9662fdfa3302636c6b8
ChapaevAnton <virsus2009@gmail.com>
2021-10-12 15:17:08 +0700

    delete unused resources

--- app/src/main/java/com/w4ereT1ckRtB1tch/moviefan/data/db/DataBaseImpl.kt
+++ app/src/main/java/com/w4ereT1ckRtB1tch/moviefan/data/db/DataBaseImpl.kt
@@ -1,14 +1,10 @@
 package com.w4ereT1ckRtB1tch.moviefan.data.db
 
-import com.w4ereT1ckRtB1tch.moviefan.App
 import com.w4ereT1ckRtB1tch.moviefan.domain.db.DataBase
 import com.w4ereT1ckRtB1tch.moviefan.domain.model.Film
 import javax.inject.Inject
 
 class DataBaseImpl @Inject constructor() : DataBase {
     private val repository = Repository()
-    init {
-     //   App.instance.appComponent.inject(this)
-    }
     override fun getDataBase(): List<Film> = repository.filmDataBase
 }
